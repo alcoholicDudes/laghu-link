2f73c0627748816106ca13b6cb1892286b79e824
mannvenders <mannvenders@dewsolutions.in>
2020-04-01 12:01:19 +0530

    add: example functional component

---
+++ frontend/src/components/Hello.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+export interface HelloProps {
+  first_name: string;
+  last_name: string;
+}
+
+const Hello = (props: HelloProps) => {
+  const { first_name = "John", last_name = "Doe" } = props;
+
+  return (
+    <h1>
+      Hello from {first_name} {last_name}!
+    </h1>
+  );
+};
+
+export default Hello;
